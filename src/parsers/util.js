80c9200513ad73cd2f68ba2f213f1adbde9c42df
kristobalus <ildar.tamendarov@gmail.com>
2023-07-18 01:01:56 +0600

    refactor: util.js

--- src/parsers/util.js
+++ src/parsers/util.js
@@ -2,7 +2,7 @@
 
 function applyRegex(regex, data) {
     regex.lastIndex = data.i;
-    var r = regex.exec(data.s);
+    const r = regex.exec(data.s);
 
     if(r && (r.index === data.i)) {
         data.i = regex.lastIndex;
